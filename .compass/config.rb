4df02273d1552a48df068a5b3fd8b1056a017adc
Rafael Torres <rafael.torres89@gmail.com>
2014-09-19 18:48:55 -0300

    Início do Framework
    Criação do Lightbox

--- .compass/config.rb
+++ .compass/config.rb
@@ -7,10 +7,10 @@ project_path = File.expand_path("..",File.dirname(__FILE__))
 
 # Important! change the paths below to match your project setup
 
-css_dir = "assets/stylesheets" # update to the path of your css files.
+css_dir = "assets/style" # update to the path of your css files.
 sass_dir = "assets/sass" # update to the path of your sass files.
-images_dir = "assets/images" # update to the path of your image files.
-javascripts_dir = "assets/javascripts" # update to the path of your script files.
+images_dir = "assets/image" # update to the path of your image files.
+javascripts_dir = "assets/libs/script" # update to the path of your script files.
 
 line_comments = false
 cache = false
